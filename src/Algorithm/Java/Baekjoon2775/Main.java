ee43bfde4c46dddaea2a38af6f421617dd75e9f1
Ssim02 <simbo@DESKTOP-2LQKBFH.localdomain>
2025-11-29 22:31:26 +0900

    [Algorithm][Baekjoon] 2775번 문제 풀이 진행 중

---
+++ src/Algorithm/Java/Baekjoon2775/Main.java
@@ -0,0 +1,29 @@
+package Algorithm.Java.Baekjoon2775;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine()); // Test case
+
+        int temp = 0; // 조건에 맞는 수를 출력하기 위해 사용할 중간 저장 변수
+        int floor = 1;
+        for(int i = 0; i < T; i++) {
+            int k = Integer.parseInt(br.readLine()); // 층 수
+            int n = Integer.parseInt(br.readLine()); // 호 수
+
+
+
+
+        }
+
+
+
+
+    }
+
+
+}
